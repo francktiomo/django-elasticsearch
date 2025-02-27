cffafecd8fd19ce85857cc383d1bfa93bad3309d
Franck Tiomo <franck.tiomo@radio-canada.ca>
2025-02-27 11:11:44 -0500

    Find a book in ES with simple match

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .documents import BookDocument
+
+
+def index(request):
+    q = request.GET.get('q')
+    context = {}
+    if q:
+        s = BookDocument.search().query('match', title=q)
+        context['books'] = s
+        print(len(s.to_queryset()))
+    return render(request, 'index.html', context)
